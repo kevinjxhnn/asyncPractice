867d0f3ec1e7b9ae3f299d5a91d22552595f5493
Kevin <kevinjxhn@Kevins-MacBook-Air.local>
2023-11-13 18:20:16 +0530

    review changes - split into functions

--- problem3/client.js
+++ problem3/client.js
@@ -1,29 +1,34 @@
 const fetch = require("node-fetch");
 
-async function main() {
-  const response = await fetch("http://localhost:3000");
-  const data = await response.json();
+async function getFilesInDir(items) {
+  const result = [];
 
-  const items = data.items
-  const result = []
-
-  for(let item of items){
-    if(item.isDir == false){
-      result.push(item.name)
-
+  for (let item of items) {
+    if (item.isDir == false) {
+      result.push(item.name);
     } else {
-      const dirResponse = await fetch(`http://localhost:3000/${item.name}`)
-      const dirData = await dirResponse.json()
-      const dirFiles = dirData.files
-      
-      for(let file of dirFiles){
-        if(file.isDir == false){
-          result.push(file.name)
+      const dirResponse = await fetch(`http://localhost:3000/${item.name}`);
+      const dirData = await dirResponse.json();
+      const dirFiles = dirData.files;
+
+      for (let file of dirFiles) {
+        if (file.isDir == false) {
+          result.push(file.name);
         }
       }
     }
   }
 
-  console.log(result)
+  return result;
 }
+
+async function main() {
+  const response = await fetch("http://localhost:3000");
+  const data = await response.json();
+
+  const items = data.items;
+
+  console.log(await getFilesInDir(items));
+}
+
 main();
